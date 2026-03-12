b34636f21ac2c431b71839d8401c5d2b844154b4
yang-summer <675229067@qq.com>
2026-03-12 15:35:42 +0800

    feat: initial commit

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,10 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import type { RouteConfig } from '@react-router/dev/routes';
+import { index, layout, route } from '@react-router/dev/routes';
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  layout('layouts/rootLayout.tsx', [index('routes/home.tsx')]),
+  layout('layouts/authLayout.tsx', [
+    route('sign-in', 'routes/_auth/signinForm.tsx'),
+    route('sign-up', 'routes/_auth/signupForm.tsx'),
+  ]),
+] satisfies RouteConfig;
